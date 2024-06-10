de89f732dd2cd102be3635f8574a746dbafffca3
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-10 01:24:27 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -157,7 +157,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
 handler.help = ['menu']
 handler.tags = ['main']
 handler.command = ['menu', 'help', 'menú'] 
-handler.register = false 
+handler.register = true 
 export default handler
 
 const more = String.fromCharCode(8206)
