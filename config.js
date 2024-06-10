48e9703ec27121ec10f571ae5ed2a9dc33e050b5
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-10 01:07:38 -0500

    Update config.js

--- config.js
+++ config.js
@@ -36,12 +36,12 @@ global.textbot = `© 2024 Starlights Team | All rights reserved`
 
 //*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷*
 
-global.group = 'https://chat.whatsapp.com/DaZrhN5469XDs6lBNmcdOO'
-global.group2 = 'https://chat.whatsapp.com/DavHccRC8t64mUUQXK4OrU'
-global.group3 = 'https://chat.whatsapp.com/GFqGm6fI8ANDaIVZ2mQciQ'
-global.group4 = 'https://chat.whatsapp.com/CqdWTXmS702JD31SQzr0Ph'
-global.group5 = 'https://chat.whatsapp.com/G0JPb1IvBwUDVFqm5YUiGA'
-global.canal = 'https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S'
+global.group = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
+global.group2 = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
+global.group3 = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
+global.group4 = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
+global.group5 = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
+global.canal = 'https://whatsapp.com/channel/0029VaBpO8M3rZZdwkGFIP33'
 
 //*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷*
 
