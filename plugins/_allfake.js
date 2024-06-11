5e880547a6321d9f01c6015c2367e2dfb5fe9772
〡ᴹⅹ〡ᴅɪᴇɢᴏ→⁶⁶⁶ <151096440+Diego-YL-177@users.noreply.github.com>
2024-06-11 11:51:33 -0500

    Actualizar plugins

---
+++ plugins/_allfake.js
@@ -0,0 +1,45 @@
+import pkg from '@whiskeysockets/baileys'
+import fs from 'fs'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import moment from 'moment-timezone'
+const { generateWAMessageFromContent, prepareWAMessageMedia, proto } = pkg
+
+var handler = m => m
+handler.all = async function (m) {
+
+global.getBuffer = async function getBuffer(url, options) {
+try {
+options ? options : {}
+var res = await axios({
+method: "get",
+url,
+headers: {
+'DNT': 1,
+'User-Agent': 'GoogleBot',
+'Upgrade-Insecure-Request': 1
+},
+...options,
+responseType: 'arraybuffer'
+})
+return res.data
+} catch (e) {
+console.log(`Error : ${e}`)
+}}
+
+let pp = ''
+let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
+//let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://telegra.ph/file/327f6ad853cb4f405aa80.jpg')
+
+const moji = (await axios.get(`https://raw.githubusercontent.com/GataNina-Li/YartexBot-MD/main/storage/juegos/emojis.json`)).data
+const emoji = await moji[Math.floor(moji.length * Math.random())]
+
+var credit = 'X8KpIFJlcXVlc3RlZCBieSBDdXJpb3NpdHlCb3Rf'
+global.cred = Buffer.from(credit, 'base64')
+
+
+
+global.fake = { contextInfo: { mentionedJid: conn.parseMention(wm), forwardingScore: 1, isForwarded: true, forwardedNewsletterMessageInfo: { newsletterJid: '120363263466636910@newsletter', serverMessageId: '', newsletterName: 'Sumi Bot 💭' }, externalAdReply: { title: packname, body: wm, mediaType: 1, renderLargerThumbnail: false, previewType: `PHOTO`, thumbnailUrl: '', thumbnail: '', sourceUrl: '' }}}, { quoted: m } //fake con reenviado de canal
+}
+
+export default handler
