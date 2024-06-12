23d73bf9e406a6add32ca90e608b316bba5cac6c
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-12 15:25:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const rl = createInterface(process.stdin, process.stdout)
 const app = express()
 const port = process.env.PORT || 8080;
 
-say('Sumi\nSakurasawa', {
+say('Sumi\nSakurasawa\n\nBot dedicado para aixa', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
