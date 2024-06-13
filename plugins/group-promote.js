89f2d2f23735d1e6f5cad6b63339f95fe21d7b15
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-13 00:18:54 -0500

    Update group-promote.js

--- plugins/group-promote.js
+++ plugins/group-promote.js
@@ -9,7 +9,7 @@ let handler = async (m, {conn, usedPrefix, text}) => {
   if (!text && !m.quoted)
     return conn.reply(
       m.chat,
-      `🚩 Use el comandó correctamente\n\n`Ejemplo :`\n\n> . promote @daniel`,
+      '🚩 Use el comandó correctamente\n\n`Ejemplo :`\n\n> . promote @daniel',
       m
     );
   if (number.length > 13 || (number.length < 11 && number.length > 0))
