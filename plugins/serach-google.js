310cc29741a8624516b47836140643ab52b1e182
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-15 01:03:43 -0500

    Update serach-google.js

--- plugins/serach-google.js
+++ plugins/serach-google.js
@@ -4,14 +4,14 @@ import axios from 'axios'
 let handler = async (m, { conn, command, args, usedPrefix }) => {
 const fetch = (await import('node-fetch')).default;
 const text = args.join` `
-if (!text) return conn.reply(m.chat, '🍭 Ingresa lo que deseas buscar en Google.', m, fakeChannel)
+if (!text) return conn.reply(m.chat, '🍭 Ingresa lo que deseas buscar en Google.', m)
 const url = 'https://google.com/search?q=' + encodeURIComponent(text)
 google({'query': text}).then(res => {
 let teks = `*🔎 Resultado de* : ${text}\n\n`
 for (let g of res) {
 teks += `*🍭 Titulo ∙* ${g.title}\n*📚 Info ∙* ${g.snippet}\n*⛓ Url ∙* ${g.link}\n\n`
 }
-conn.reply(m.chat, teks, m, fake,)
+conn.reply(m.chat, teks, m)
 })
 }
 handler.help = ['google <búsqueda>']
