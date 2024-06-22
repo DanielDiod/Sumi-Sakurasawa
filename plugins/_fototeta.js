3563ea596c7b4f80a9a3e02e276032fbb9496968
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-22 02:10:39 -0500

    Update _fototeta.js

--- plugins/_fototeta.js
+++ plugins/_fototeta.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, usedPrefix, command}) => {
-await conn.reply(m.chat,`Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal.`, estilo)
+await conn.reply(m.chat,`Padre nuestro, que estás en los Cielos, santificado sea tu nombre, venga tu Reino, hágase tu voluntad así en la tierra como en el cielo. y perdónanos nuestras deudas así como nosotros perdonamos a nuestros deudores, y no nos dejes caer en la tentación, mas líbranos del mal.`, m)
 }
 handler.customPrefix = /^(Fototeta|fotopoto)$/i
 handler.command = new RegExp
