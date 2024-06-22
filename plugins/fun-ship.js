33cf77aafc68862a5bad5782206a13813e3e2ba0
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-22 02:52:54 -0500

    Update fun-ship.js

--- plugins/fun-ship.js
+++ plugins/fun-ship.js
@@ -1,10 +1,10 @@
 var handler = async (m, { conn, command, text }) => {
 
-if (!text) return conn.reply(m.chat, `🎌 *Escribe el nombre de dos personas para calcular su amor*`, m, null, rcanal)
+if (!text) return conn.reply(m.chat, `🎌 *Escribe el nombre de dos personas para calcular su amor*`, m)
 let [text1, ...text2] = text.split(' ')
 
 text2 = (text2 || []).join(' ')
-if (!text2) return conn.reply(m.chat, `🚩 *Escribe el nombre de la segunda persona*`, m, null, rcanal)
+if (!text2) return conn.reply(m.chat, `🚩 *Escribe el nombre de la segunda persona*`, m)
 let love = `❤️ *${text1}* tu oportunidad de enamorarte de *${text2}* es de *${Math.floor(Math.random() * 100)}%* 👩🏻‍❤️‍👨🏻`
 
 m.reply(love, null, { mentions: conn.parseMention(love) })
