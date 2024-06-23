1ae8a8e38f94147b38b0aeaca2af52addf957df1
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-23 00:58:21 -0500

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -2,21 +2,24 @@ import moment from 'moment-timezone'
 import fetch from 'node-fetch'
 
 let handler = async (m, { conn, args }) => {
-   let res = await fetch('https://api.github.com/repos/DanielDiod/Sumi-Sakurasawa')
-   let json = await res.json()
-   let img = await (await fetch(`https://tinyurl.com/258rd289`)).buffer()
-   let txt = `*B O T  -  S C R I P T*\n\n`
-      txt += `	◦  *Nombre* : ${json.name}\n`
-      txt += `	◦  *Visitas* : ${json.watchers_count}\n`
-      txt += `	◦  *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
-      txt += `	◦  *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
-      txt += `	◦  *Url* : ${json.html_url}\n`
-      txt += `	◦  *Forks* : ${json.forks_count}\n`
-      txt += `	◦  *Stars* : ${json.stargazers_count}`
-   await conn.Sumi(m.chat, botname, txt, img, img, canal, m)
+let res = await fetch('https://api.github.com/repos/DanielDiod/Sumi-Sakurasawa')
+let json = await res.json()
+try {
+let txt = `*乂  S C R I P T  -  M A I N*\n\n`
+    txt += `	✩   *Nombre* : ${json.name}\n`
+    txt += `	✩   *Visitas* : ${json.watchers_count}\n`
+    txt += `	✩   *Peso* : ${(json.size / 1024).toFixed(2)} MB\n`
+    txt += `	✩   *Actualizado* : ${moment(json.updated_at).format('DD/MM/YY - HH:mm:ss')}\n`
+    txt += `	✩   *Url* : ${json.html_url}\n`
+    txt += `	✩   *Forks* : ${json.forks_count}\n`
+    txt += `	✩   *Stars* : ${json.stargazers_count}\n\n`
+    txt += `> 🚩 *${textbot}*`
+let img = await (await fetch(`https://i.ibb.co/LQKxczm/file.jpg`)).buffer()
 
-}
-
+await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
+} catch {
+await m.react('✖️')
+}}
 handler.help = ['script']
 handler.tags = ['main']
 handler.command = ['script', 'sc']
