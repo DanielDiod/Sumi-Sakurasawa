6c0efd3f21c0f690bd2468fd0dcfdb29f7f1a3c4
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-25 20:04:15 -0500

    Create rg-unreg.js

---
+++ plugins/rg-unreg.js
@@ -0,0 +1,16 @@
+import { createHash } from 'crypto'
+let handler = async function (m, { conn, args, usedPrefix}) {
+  if (!args[0]) return m.reply(`🍭 Ingresa tu número de serie junto al comando.`)
+  let user = global.db.data.users[m.sender]
+  let sn = createHash('md5').update(m.sender).digest('hex')
+  if (args[0] !== sn) return m.reply('🍭 Número de serie incorrecto')
+  user.registered = false
+  m.reply(`🍭 Registro eliminado con éxito.`)
+}
+handler.help = ['unreg'] 
+handler.tags = ['rg']
+
+handler.command = ['unreg'] 
+handler.register = true
+
+export default handler
