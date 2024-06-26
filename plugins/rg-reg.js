abad170cd97516a071c12c064fcb7f6790a7fd4a
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-25 19:46:59 -0500

    Update rg-reg.js

--- plugins/rg-reg.js
+++ plugins/rg-reg.js
@@ -1,31 +1,38 @@
+import db from '../lib/database.js'
+import { createHash } from 'crypto'
 import fs from 'fs'
 import fetch from 'node-fetch'
 
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { conn, text, usedPrefix, command }) {
-  let tr = '🍭 Ingresa tu nombre y tu edad.\n\n`Ejemplo:`\n' + `> *${usedPrefix + command}* Sumi.19`
   let user = global.db.data.users[m.sender]
   let name2 = conn.getName(m.sender)
-  if (user.registered === true) return m.reply(`🍭 Ya estás registrado.`)
-  if (!Reg.test(text)) return m.reply(tr)
+  if (user.registered === true) return m.reply(`🍭 Ya estás registrado.\n\n*¿Quiere volver a registrarse?*\n\nUse este comando para eliminar su registro.\n*${usedPrefix}unreg* <Número de serie>`)
+  if (!Reg.test(text)) return m.reply(`🍭 Formato incorrecto.\n\nUso del comamdo: *${usedPrefix + command} nombre.edad*\nEjemplo : *${usedPrefix + command} ${name2}.16*`)
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) return m.reply(tr)
-  if (!age) return m.reply(tr)
+  if (!name) return m.reply('🍭 El nombre no puede estar vacío.')
+  if (!age) return m.reply('🍭 La edad no puede estar vacía.')
+  if (name.length >= 100) return m.reply('🍭 El nombre es demasiado largo.' )
   age = parseInt(age)
-  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj.')
+  if (age > 100) return m.reply('👴🏻 Wow el abuelo quiere jugar al bot.')
+  if (age < 5) return m.reply('🚼  hay un abuelo bebé jsjsj. ')
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
+  let sn = createHash('md5').update(m.sender).digest('hex')
   let img = await (await fetch(`https://tinyurl.com/258rd289`)).buffer()
-  let txt = `╭─⬣「 *User Registro* 」⬣\n`
-    txt += `│  ≡◦ *🪴 Nombre ∙* ${name}\n`
-    txt += `│  ≡◦ *🐢 Edad ∙* ${age} años\n`
-    txt += `╰─⬣`
-await conn.Sumi(m.chat, botname, txt, img, img, canal, m)
+  let txt = '`–  *R E G I S T R O  -  U S E R`\n\n'
+      txt += `┌  ✩  *Nombre* : ${name}\n`
+      txt += `│  ✩  *Edad* : ${age} años\n`
+      txt += `│  ✩  *Numero de serie*\n`
+      txt += `└  ✩  ${sn}`
+await conn.sendAi(m.chat, botname, textbot, txt, img, img, canal, m)
+await m.react('✅')
 }
-handler.help = ['reg <nombre.edad>']
+handler.help = ['reg'].map(v => v + ' *<nombre.edad>*')
 handler.tags = ['rg']
+
 handler.command = ['verify', 'reg', 'register', 'registrar'] 
 
 export default handler
