b673068359d11ebdd88582337f06398b76b058ed
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-06-29 00:45:18 -0500

    Update youtube-play.js

--- plugins/youtube-play.js
+++ plugins/youtube-play.js
@@ -26,7 +26,7 @@ const texto1 = `乂  Y O U T U B E   M U S I C\n
         ✩ *Publicado ∙* ${vid.ago}\n
         ✩ *Url ∙* ${'https://youtu.be/' + vid.videoId}\n`.trim()
 		
-await conn.sendButton(m.chat, texto1, wm, res.videos[0].thumbnail, [
+await conn.sendButton2(m.chat, texto1, wm, res.videos[0].thumbnail, [
 	['Audio 📀', `${usedPrefix}mp3 ${text}`],
 	['Video 🎥', `${usedPrefix}mp4 ${text}`]
   ], null, [['Canal', `${md}`]], m)
