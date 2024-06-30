cacd00ace4cccbbc6d717b1ebc92990f641f4f5c
Zam <141449338+AzamiJs@users.noreply.github.com>
2024-06-29 22:07:31 -0600

    Update _allfake.js

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -6,6 +6,19 @@ let img = await (await fetch(`https://tinyurl.com/2c5hk765`)).buffer()
  global.rcanal = {
     contextInfo: {
     	isForwarded: true,
+	    externalAdReply: {
+				    showAdAttribution: true,
+					title: botname,
+					body: textbot,
+					mediaUrl: null,
+					description: null,
+					previewType: "PHOTO",
+					thumbnailUrl: img,
+                    thumbnail: img,
+		           sourceUrl: canal,
+		           mediaType: 1,
+                   renderLargerThumbnail: true
+	    },
     forwardedNewsletterMessageInfo: {
       newsletterJid: "120363206717994793@newsletter",
       serverMessageId: 100,
