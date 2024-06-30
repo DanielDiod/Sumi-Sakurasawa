105aca72bdeba65a35da0a309d5a6ac010a24c89
Zam <141449338+AzamiJs@users.noreply.github.com>
2024-06-29 22:48:12 -0600

    Sumi

--- plugins/_allfake.js
+++ plugins/_allfake.js
@@ -18,7 +18,7 @@ let img = catalogo
 					mediaUrl: null,
 					description: null,
 					previewType: "PHOTO",
-					thumbnailUrl: 'https://telegra.ph/file/ddf0ec3c46014b5d9f98a.jpg',
+					thumbnailUrl: 'https://telegra.ph/file/474de03b8c0cf87231d8e.jpg',
 		           sourceUrl: canal,
 		           mediaType: 1,
                    renderLargerThumbnail: false
