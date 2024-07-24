3ee0ac2699b95017bf661461acc14aeafe8a7ffc
DanielDiod <153774329+DanielDiod@users.noreply.github.com>
2024-07-24 14:07:27 -0500

    Create fun-abrazar.js

---
+++ plugins/fun-abrazar.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+
+const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
+    
+ let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+else who = m.chat
+if (!who) throw `*[❗] Por favor, menciona el usuario*`
+  if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    
+ let pp = "https://telegra.ph/file/4d80ab3a945a8446f0b15.mp4"
+let pp2 = "https://telegra.ph/file/ef3a13555dfa425fcf8fd.mp4"
+let pp3 = "https://telegra.ph/file/582e5049e4070dd99a995.mp4"
+let pp4 = "https://telegra.ph/file/ab57cf916c5169f63faee.mp4"
+let pp5 = "https://telegra.ph/file/fce96960010f6d7fc1670.mp4"
+let pp6 = "https://telegra.ph/file/33332f613e1ed024be870.mp4"
+
+  try {
+    const locale = 'es-ES';
+    const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
+    const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
+    const document = doc[Math.floor(Math.random() * doc.length)];
+    const str = `
+
+        _${taguser} HA ABRAZADO A @${who.split`@`[0]} 🫂 🤗_
+ 
+  
+    `.trim();
+    if (m.isGroup) {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
+    } else {
+      const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
+      conn.sendMessage(m.chat, { video: { url: [pp, pp2, pp3, pp4, pp5, pp6].getRandom() }, gifPlayback: true, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
+    }
+  } catch {
+    conn.reply(m.chat, '*[❌] ocurrio un error inesperado*', m);
+  }
+};
+handler.help = ['hug'].map((v) => v + ' <@usuario>');
+handler.tags = ['fun'];
+handler.command = /^(abrazar)$/i;
+handler.register = true;
+export default handler;
